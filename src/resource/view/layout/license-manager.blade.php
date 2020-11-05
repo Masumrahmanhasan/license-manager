ac8bb2c76cd2f22ac8aacb91dc35fbc457c56362
Alfath Dioni <fathalfath30@gmail.com>
2020-11-05 21:15:13 +0700

    update base template

--- src/resource/view/layout/license-manager.blade.php
+++ src/resource/view/layout/license-manager.blade.php
@@ -13,4 +13,22 @@
 //  Github : https://github.com/fathalfath30
 //  Gitlab : https://gitlab.com/Fathalfath30
 //
---}}
+--}}<!DOCTYPE html>
+<html lang='{{ app()->getLocale() }}'>
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <title>{{ env('APP_NAME', 'Fathalfath30') }} :: License Manager</title>
+
+  <link rel='stylesheet' href='{{ asset('license-manager/vendor/bootstrap/css/bootstrap.min.css') }}' />
+</head>
+<body>
+
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script type='text/javascript' src='{{ asset('license-manager/vendor/jquery/jquery-3.2.1.slim.min.js') }}'></script>
+<script type='text/javascript' src='{{ asset('license-manager/vendor/popper/popper.min.js') }}'></script>
+<script type='text/javascript' src='{{ asset('license-manager/vendor/bootstrap/js/bootstrap.min.js') }}'></script>
+</body>
+</html>
